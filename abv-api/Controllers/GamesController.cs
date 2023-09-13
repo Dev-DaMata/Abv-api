7771253428f29087c5eb43aec69af7793c7c8fa2
GuilhermeCordeiroDaMata <gcordeiro773@gmail.com>
2023-09-13 17:40:46 -0300

    :sperkles: feat: Configurado as injeções de dependências e a rota GET da entidade Games

--- abv-api/Controllers/GamesController.cs
+++ abv-api/Controllers/GamesController.cs
@@ -1,12 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using abv_api.Model;
+using abv_api.Repository;
+using System.Reflection;
 
 namespace abv_api.Controllers
 {
     public class GamesController : Controller
     {
-        public IActionResult Index()
+        #region Injeção de Dependências
+        private readonly IConfiguration _config;
+
+        private readonly ILogger<GamesController> _logger;
+
+        private readonly GamesRepository _gamesRepository;
+
+        public GamesController(IConfiguration config, ILogger<GamesController> logger, GamesRepository gamesRepository)
         {
-            return View();
+            _config = config;
+            _logger = logger;
+            _gamesRepository = gamesRepository;
         }
+
+        #endregion
+
+        #region GET
+        [HttpGet]
+        [Route("GetGames")]
+        public async Task<ActionResult> GetGames()
+        {
+            try
+            {
+                var data = await _gamesRepository.GetGames();
+                return Ok(data);
+            }catch (Exception ex)
+            {
+                _logger.LogError(ex, "GetGames: Erro na requisição dos dados");
+                return new StatusCodeResult(500);
+            }
+        }
+        #endregion
     }
 }
--- abv-api/Repository/GamesRepository.cs
+++ abv-api/Repository/GamesRepository.cs
@@ -1,6 +1,36 @@
-﻿namespace abv_api.Repository
+﻿using Dapper;
+using abv_api.Model;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace abv_api.Repository
 {
     public class GamesRepository
     {
+        #region Injeção de Dependências
+        private readonly IConfiguration _config;
+
+        public GamesRepository(IConfiguration config)
+        {
+            _config = config;
+        }
+        #endregion
+
+        #region GET
+        public async Task<List<Games>> GetGames()
+        {
+            var connection = _config.GetConnectionString("DefaultConnection");
+            var Instance = new SqlConnection(connection);
+            var query = "SELECT * FROM Games";
+            var response = Instance.Query<Games>(query).ToList();
+            return response;
+        }
+        #endregion
     }
 }
